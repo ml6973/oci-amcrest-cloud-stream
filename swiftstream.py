15fc78abfbe4bf982c90120884449ff35c8cf7b4
gregoryhooks <gregoryhooksjr@gmail.com>
2017-05-15 14:21:25 -0500

    Switched to live555 library for streaming from cameras

--- swiftstream.py
+++ swiftstream.py
@@ -3,6 +3,7 @@ import chameleon.chameleonAuth as chameleonAuth
 import configuration.globalVars as globalVars
 import datetime
 import libraries.BufferQueue as BufferQueue
+import live555
 import multiprocessing
 import requests
 import shutil
@@ -16,30 +17,49 @@ def send_data(data, url, my_headers):
    r = requests.put(url, data=data, headers=my_headers)
 
 # Takes the data from the camera and put it in the buffer (producer)
-def fill_queue(data, queue):
+def fill_queue(codecName, bytes, sec, usec, durUSec):
+   # Check that we are saving only the video frames
+   if not (codecName == 'H264'):
+      return
+
+   # Each frame must start with this byte sequence
+   bytes = b'\0\0\0\1' + bytes
    
-   bytecount = 0
+   # Use the global variables initialized by the director process
+   global bytecount 
+   global q
+
+   amount = min(len(bytes), globalVars.uploadSize, (globalVars.uploadSize - bytecount))
+   q.put(bytes[:amount])
+   bytecount += amount
+
+   if bytecount >= globalVars.uploadSize:
    
-   #Loop until the uploadSize amount of bytes have been put into the buffer
-   while bytecount < globalVars.uploadSize:
-      #Stop filling the buffer if keyboard interrupt was detected
-      if quitEvent.is_set():
-         break
-      amount = min(1000, globalVars.uploadSize, (globalVars.uploadSize - bytecount))
-      queue.put(data.read(amount))
-      bytecount += amount
-   
-   #Close the buffer so that the consumer knows there is no more data
-   queue.close()
+      #Close the buffer so that the consumer knows there is no more data
+      q.close()
+
+      #Put remainder of frame into the queue
+      q.put(bytes[amount:])
+      bytecount = len(bytes) - amount
 
 # Initiates the camera feed and spawns the consumer / producer threads
 def realtime_swift_stream(self, channel=1, typeno=0):
-   ret = self.command(
-      'realmonitor.cgi?action=getStream&channel={0}&subtype={1}'.format(
-         channel, typeno)
-   )
 
    stored_exception=None
+
+   # Create a new buffer for streaming
+   global q
+   q = BufferQueue.BufferQueue(globalVars.bufferSize)
+
+   # Counter to maintain upload size
+   global bytecount
+   bytecount = 0
+
+   # Start the producer thread
+   live555.startRTSP(cameraURL, fill_queue, False)
+   producer = threading.Thread(target=live555.runEventLoop)
+   producer.setDaemon(True)
+   producer.start()
 
    # Loop until the break signal has been received
    try:
@@ -50,31 +70,29 @@ def realtime_swift_stream(self, channel=1, typeno=0):
          # Setup the filename for the uploaded segment
          ts = time.time()
          fileName = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-         fileName = camera.machine_name[5:].strip() + " " + fileName
+         fileName = cameraName + " " + fileName
 
          # Retrieve authentication for swift and setup storage location
          my_token_id = chameleonAuth.auth(tenantName)
          url = globalVars.chameleonObjectStorageURL + "/" + containerName + "/" + path + "/" + fileName
          my_headers = {"Content-Type": 'binary/octet-stream', "Transfer-Encoding": "chunked", "X-Auth-Token": my_token_id}
-
-         # Create a new buffer for streaming
-         q = BufferQueue.BufferQueue(globalVars.bufferSize)
-
+         
          consumer = threading.Thread(target=send_data, args=(q, url, my_headers,))
-         producer = threading.Thread(target=fill_queue, args=(ret.raw, q,))
 
          consumer.start()
-         producer.start()
 
          #Wait for upload to complete, timeout in while loop allows keyboard interrupt to function
-         while producer.is_alive() or consumer.is_alive():
-            producer.join(timeout=1.0)
+         while consumer.is_alive():
             consumer.join(timeout=1.0)
 
    except KeyboardInterrupt:
       stored_exception=sys.exc_info()
       quitEvent.set()
-      print("Camera " + camera.machine_name[5:] + " finished.")
+      live555.stopEventLoop()
+      producer.join()
+      q.close()
+      consumer.join()
+      print("Camera " + cameraName + " finished.")
 
 # Wrapper so that this function is spawned in another process
 def stream_video():
@@ -100,10 +118,13 @@ if __name__ == '__main__':
          path = path.lstrip("/")
          path = path.rstrip("/")
 
+         cameraURL = "rtsp://" + camera['username'] + ":" + camera['password'] + "@" + camera['hostname'] + ":554/cam/realmonitor?channel=1&subtype=0"
+
          camera = amcrest.camera
+         cameraName = camera.machine_name[5:].strip()
 
          camera.realtime_swift_stream = types.MethodType( realtime_swift_stream, camera )
-         processes.append(multiprocessing.Process(target=stream_video, name="streamvideo"+camera.machine_name[5:], args=()))
+         processes.append(multiprocessing.Process(target=stream_video, name="streamvideo"+cameraName, args=()))
          processes[-1].start()
 
       for process in processes:
